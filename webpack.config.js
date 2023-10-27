631485003f992075d2be5ed59b349371564aa31f
Real-Chaos <rizwannafees13@gmail.com>
2023-10-27 12:27:08 -0400

    set up the project

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(?:js|mjs|cjs)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [['@babel/preset-env', { targets: 'defaults' }]],
+					},
+				},
+			},
+		],
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/template.html',
+			filename: 'index.html',
+			inject: 'body',
+		}),
+	],
+}
